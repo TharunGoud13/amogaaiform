29f139f65cb77be7343377768fccc5c15b2a18df
TharunGoud13 <amaravaitharun13@gmail.com>
2024-11-18 13:54:59 +0530

    added base url in env

--- src/app/actions/generateForm.ts
+++ src/app/actions/generateForm.ts
@@ -34,12 +34,14 @@ export async function generateForm(
   console.log(data);
 
   try {
-    console.log("indside");
+    console.log("inside");
     const prompt = `${data.description} Based on the description, generate a survey object with 3 fields: name(string) for the form, description(string) of the form and a questions array where every element has 2 fields: text and the fieldType and fieldType can be of these options RadioGroup, Select, Input, Textarea, Switch; and return it in json format. For RadioGroup, and Select types also return fieldOptions array with text and value fields. And more importantly, questions should be only 2 in number. For example, for RadioGroup, and Select types, the field options array can be [{text: 'Yes', value: 'yes'}, {text: 'No', value: 'no'}] and for Input, Textarea, and Switch types, the field options array can be empty. For example, for Input, Textarea, and Switch types, the field options array can be []`;
     const result = await model.generateContent(prompt);
     const response = await result.response;
+    console.log("response----",response)
     const text = response.text();
     const jsonString = text.replace(/^```json\s*([\s\S]*)\s*```$/g, "$1");
+    console.log("jsonString----", jsonString);
 
     const responseObject = JSON.parse(jsonString);
     console.log(responseObject, "dsf");
